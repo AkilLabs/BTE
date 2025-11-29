62540da9f200135eb739695e7d870ce20b685a76
AKIL A <akilaskarali@gmail.com>
2025-11-29 23:55:59 +0530

    Add user and admin authentication views with JWT support

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,13 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = []
+urlpatterns = [
+
+    #User
+    path("user_signup/", user_signup, name="user_signup"),
+    path("user_login/", user_login, name="user_login"),
+
+    #Admin
+    path("admin_signup/", admin_signup, name="admin_signup"),
+    path("admin_login/",admin_login, name="admin_login"),
+]
