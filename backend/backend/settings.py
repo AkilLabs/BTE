db711a37c0051c49b26268e9d188a9ef858e4f3e
AKIL A <akilaskarali@gmail.com>
2025-12-02 13:59:18 +0530

    FEAT: Update CORS_ALLOWED_ORIGINS to include production server IP

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,7 +134,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
-    "http://localhost:5173",
+    "http://68.183.80.191:5173",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
